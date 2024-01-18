81bb3563521158ddb8e6a559895b6b62c7852234
jonhyble <jonhy3-valadez@hotmail.com>
2024-01-18 00:16:46 -0600

    Need to change select to put styles

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -3,10 +3,11 @@ import { useEffect } from "react";
 import './index.css'
 import Lang from "../Lang"
 
-const isActive = ({ isActive }) => `flex-1 whitespace-nowrap border-r-2 px-1 py-4 text-xl font-medium text-center w-1/3 ${isActive ? "active" : "text-white"}`;
+const isActive = ({ isActive }) => `flex-1 whitespace-nowrap border-r-2 px-1 py-4 text-xl font-medium text-center w-1/3 container-nav ${isActive ? "active" : "text-white unactive"}`;
 
 export const Menu = () => {
   const pageTitle = Lang("page-title");
+  const lng = localStorage.getItem("lng") || "en";
 
   useEffect(() => {
       document.title = pageTitle;
@@ -23,20 +24,20 @@ export const Menu = () => {
   }
 
   const listItems = options.map((option) =>
-    <option key={option.value} value={option.value}>{option.label}</option>
+    <option key={option.value} value={option.value} className={`text-xl ${(option.value === lng) ? "lng-selected" : "option-no-selected"}`}>{option.label}</option>
   );
 
   return (
       <nav className="w-screen h-fit">
-          <div className="border-b border-gray-200 -mb-px flex space-x-8 px-4">
+          <div className="border-b border-gray-200 -mb-px flex">
               <NavLink className={isActive} to="/home">
                   {Lang("home")}
               </NavLink>
               <NavLink className={isActive} to="/projects">
                   {Lang("projects")}
               </NavLink>
-              <div className="w-1/3 grid place-items-center">
-                <select id="lngSelect" className="w-fit text-center" defaultValue={localStorage.getItem("lng") || "en"} onChange={handleSelectChange}>
+              <div className="w-1/3 grid place-items-center container-nav">
+                <select id="lngSelect" className="w-full h-full text-center bg-transparent font-medium text-xl" defaultValue={localStorage.getItem("lng") || "en"} onChange={handleSelectChange}>
                   {listItems}
                 </select>
               </div>
