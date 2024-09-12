03732930881d8a780425fc967b71ce16af5791c9
Jonhyble <jonhy3-valadez@hotmail.com>
2024-09-11 21:36:43 -0600

    Added 2 projects

--- src/components/Popup/index.jsx
+++ src/components/Popup/index.jsx
@@ -4,15 +4,25 @@ import Lang from "../../components/Lang"
 
 export const Popup = ({ popup, setPopup }) => {
     const [content, setContent] = useState(0);
+    const [image, setImage] = useState(0);
 
     const projectsImages = {
         "visits" : [
             "/visits/home_page.png",
-            ""
+            "/visits/01.png"
         ],
-        "salesSystem" : [
-            "",
-            ""
+        "repruebainador" : [
+            "/repruebainador/home_page.png",
+            "/repruebainador/01.png"
+        ]
+    }
+
+    const technologies = {
+        "visits" : [
+            "React"
+        ],
+        "repruebainador" : [
+            "Python"
         ]
     }
 
@@ -34,17 +44,38 @@ export const Popup = ({ popup, setPopup }) => {
     const closePop = (e) => {
         e.preventDefault();
         setPopup("");
-        console.log(projectsImages[popup]);
+        setContent(0);
+        setImage(0);
     }
 
-    function PlaceContent() {
-        if (content === 0) {
-            return <div className="w-full h-5/6 bg-blue-200">Imagenes</div>
-        } else if (content === 1) {
-            return <div className="w-full h-5/6 bg-blue-200">Descripcion</div>
-        } else {
-            return <div className="w-full h-5/6 bg-blue-200">Tecnologias</div>
+    const changeImage = (e) => {
+        e.preventDefault();
+        if (e.currentTarget.value === "true") {
+            if (image >= projectsImages[popup].length - 1)
+                setImage(0);
+            else 
+                setImage(image + 1);
         }
+        else if (image <= 0){
+            setImage(projectsImages[popup].length - 1);
+        } else {
+            setImage(image - 1);
+        }
+    }
+
+    const listItems = technologies[popup]?.map((option) =>
+        <p key={option} className="text-lg font-bold text-center flex-none w-36 mx-5 mb-6 p-3 border-2 border-blue-400 bg-blue-500 hover:opacity-70 text-white rounded-3xl">{option}</p>
+    );
+
+    function PlaceContent() {
+        if(popup !== "")
+            if (content === 0) {
+                return <div className="w-full h-full bg-blue-200"><button onClick={changeImage} value={false} className="absolute left-0 h-full w-1/12 text-9xl bg-blue-300/75">{"<"}</button><button onClick={changeImage} value={true} className="absolute right-0 h-full w-1/12 text-9xl bg-blue-300/75">{">"}</button><img className="m-auto max-h-full max-w-full" src={projectsImages[popup][image]} alt="image" /></div>
+            } else if (content === 1) {
+                return <div className="w-full h-fit bg-blue-200 text-blue-800 m-auto text-center absolute inset-y-1/2 text-4xl">{Lang(popup + "_description")}</div>
+            } else {
+                return  <div className="flex flex-wrap justify-center absolute inset-1/2">{listItems}</div>
+            }
     }
     
     return (
@@ -52,7 +83,7 @@ export const Popup = ({ popup, setPopup }) => {
             <div className="w-screen h-screen fixed bg-blue-800 opacity-50 top-0 left-0 right-0"/>
             <button onClick={closePop} className="fixed top-0 mr-4 text-4xl right-0 hover:text-sky-200">X</button>
             <div className="w-5/6 h-5/6 absolute place-self-center bg-blue-500">
-                <div className="w-full h-5/6 bg-blue-200"><PlaceContent/></div>
+                <div className="w-full h-5/6 bg-blue-200 relative"><PlaceContent/></div>
                 <div className="w-full flex justify-around h-1/6 bg-blue-800">
                     <button onClick={imagesOnClick} className="bg-blue-500 h-full w-full border border-sky-500">{Lang("button_images")}</button>
                     <button onClick={descriptionOnClick} className="bg-blue-500 h-full w-full border border-sky-500">{Lang("button_description")}</button>
--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -21,17 +21,9 @@ export const Projects = () => {
                         <p>{Lang("visits_title")}</p>
                         <img src="/visits/home_page.png" alt="image" />
                     </button>
-                    <button onClick={handleClick} value="visits" className="text-lg font-bold text-center flex-none lg:w-[35rem] md:w-[40rem] w-5/6 h-[24rem] mx-5 mt-6 p-3 border-2 border-blue-400 bg-blue-500 hover:opacity-70 text-white rounded-3xl">
-                        <p>{Lang("visits_title")}</p>
-                        <img src="/visits/home_page.png" alt="image" />
-                    </button>
-                    <button onClick={handleClick} value="visits" className="text-lg font-bold text-center flex-none lg:w-[35rem] md:w-[40rem] w-5/6 h-[24rem] mx-5 mt-6 p-3 border-2 border-blue-400 bg-blue-500 hover:opacity-70 text-white rounded-3xl">
-                        <p>{Lang("visits_title")}</p>
-                        <img src="/visits/home_page.png" alt="image" />
-                    </button>
-                    <button onClick={handleClick} value="visits" className="text-lg font-bold text-center flex-none lg:w-[35rem] md:w-[40rem] w-5/6 h-[24rem] mx-5 mt-6 p-3 border-2 border-blue-400 bg-blue-500 hover:opacity-70 text-white rounded-3xl">
-                        <p>{Lang("visits_title")}</p>
-                        <img src="/visits/home_page.png" alt="image" />
+                    <button onClick={handleClick} value="repruebainador" className="text-lg font-bold text-center flex-none lg:w-[35rem] md:w-[40rem] w-5/6 h-[24rem] mx-5 mt-6 p-3 border-2 border-blue-400 bg-blue-500 hover:opacity-70 text-white rounded-3xl">
+                        <p>{Lang("repruebainador_title")}</p>
+                        <img src="/repruebainador/home_page.png" alt="image" />
                     </button>
                 </div>
             </main>
